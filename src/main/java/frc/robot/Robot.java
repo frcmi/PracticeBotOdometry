d4b9a510f141843c3c0945c47ae07ea4d92d626a
Daniel Bereza <blackcatmaxy@gmail.com>
2022-10-13 16:23:01 -0700

    Simplify telop drive code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -94,7 +94,10 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    m_robotContainer.m_robotDrive.arcadeDrive(-m_robotContainer.m_driverController.getLeftY(), -m_robotContainer.m_driverController.getRightX());
+    double fwd = -m_robotContainer.m_driverController.getLeftY() * 0.5;
+    double rot = -m_robotContainer.m_driverController.getRightX() * 0.5;
+
+    m_robotContainer.m_robotDrive.arcadeDrive(yAxis, xAxis);
   }
 
   @Override
